e7c492142ddc61ea765c2e962fc463529141232f
Caio <whoiscaio@protonmail.com>
2022-02-19 11:13:51 -0300

    feat: routes created

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send('<h1>GET /goals</h1>')
+  res.status(200).send('<h1>GET /goals</h1>');
 });
+
+router.get('/:id', (req, res) => {
+  res.status(200).send('<h1>GET /goals/:id</h1>');
+})
+
+router.post('/', (req, res) => {
+  res.status(200).send('<h1>POST /goals</h1>');
+})
+
+router.patch('/:id', (req, res) => {
+  res.status(200).send('<h1>PATCH /goals/:id</h1>');
+})
+
+router.delete('/:id', (req, res) => {
+  res.status(200).send('<h1>DELETE /goals/:id</h1>');
+})
 
 module.exports = router;
