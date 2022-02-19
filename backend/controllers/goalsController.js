183cb5bbc4d9799b4f4326696bc9763f5ab9d617
Caio <whoiscaio@protonmail.com>
2022-02-19 15:22:36 -0300

    feat: controller error throwing added

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -9,13 +9,33 @@ async function getGoals(req, res) {
 async function getGoal(req, res) {
   const { id } = req.params;
 
+  if(id.length !== 24) {
+    res.status(400);
+    throw new Error('Invalid id provided');
+  }
+
   const goal = await goalModel.findById(id);
+
+  if(!goal) {
+    res.status(404);
+    throw new Error('Goal not found');
+  }
 
   res.status(200).json(goal);
 }
 
 async function createGoal(req, res) {
   const body = req.body;
+
+  if(!body.text) {
+    res.status(400);
+    throw new Error('Text field is required');
+  }
+
+  if(!body.completed) {
+    res.status(400);
+    throw new Error('Completed field is required');
+  }
 
   const newGoal = await goalModel.create(body);
 
@@ -26,9 +46,21 @@ async function updateGoal(req, res) {
   const { id } = req.params;
   const body = req.body;
 
-  const updatedUser = await goalModel.findByIdAndUpdate(id, body, { new: true });
+  if(id.length !== 24) {
+    res.status(400);
+    throw new Error('Invalid id provided');
+  }
 
-  res.status(200).json(updatedUser);
+  const goal = await goalModel.findById(id);
+
+  if(!goal) {
+    res.status(404);
+    throw new Error('Goal not found');
+  }
+
+  const updatedGoal = await goalModel.findByIdAndUpdate(id, body, { new: true });
+
+  res.status(200).json(updatedGoal);
 }
 
 async function deleteGoal(req, res) {
