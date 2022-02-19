86b9b6d583352b08eea206e2bddbe43601c56c07
Caio <whoiscaio@protonmail.com>
2022-02-19 12:31:32 -0300

    feat: get goal by id and get all goals routes created

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,20 +1,36 @@
-function getGoals(req, res) {
-  res.status(200).send('<h1>GET /goalssss</h1>');
+const goalModel = require('../models/goal.model');
+
+async function getGoals(req, res) {
+  const goals = await goalModel.find({});
+
+  res.status(200).json(goals);
 }
 
-function getGoal(req, res) {
-  res.status(200).send('<h1>GET /goals/:id</h1>');
+async function getGoal(req, res) {
+  const { id } = req.params;
+
+  if(id.length !== 24) {
+    return res.status(400).json({ error: "Invalid id was provided" });
+  }
+
+  const goal = await goalModel.findById(id);
+
+  if(!goal) {
+    return res.status(404).json({ error: "Goal not found" });
+  }
+
+  res.status(200).json(goal);
 }
 
-function createGoal(req, res) {
+async function createGoal(req, res) {
   res.status(200).send('<h1>POST /goals</h1>');
 }
 
-function updateGoal(req, res) {
+async function updateGoal(req, res) {
   res.status(200).send('<h1>PUT /goals/:id</h1>');
 }
 
-function deleteGoal(req, res) {
+async function deleteGoal(req, res) {
   res.status(200).send('<h1>DELETE /goals/:id</h1>');
 }
 
