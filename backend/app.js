746186cb9e355d939c9c2f7d259c44d6c80b2051
Caio <whoiscaio@protonmail.com>
2022-02-19 21:10:38 -0300

    feat: error handler middleware added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const connect = require('./database/connect');
+const { errorHandler } = require('./middleware/errorHandler');
 const goalRoutes = require('./routes/goalRoutes');
 
 connect();
@@ -9,8 +10,10 @@ connect();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(express.json());
 app.use('/api/goals', goalRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server start at: http://localhost.com:${PORT}`);
