57195d9143ba4bbe813f0a6ff99558298f10ef06
Caio <whoiscaio@protonmail.com>
2022-02-19 22:07:15 -0300

    feat: mongodb uri handling completely handed to env variables

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGODB_ADDRESS = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@goalscluster.3sfzl.mongodb.net/goalsDb?retryWrites=true&w=majority`;
-
 async function connect() {
-  await mongoose.connect(MONGODB_ADDRESS, (err) => {
+  await mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (err) {
       return console.log(err);
     }
