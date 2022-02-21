a0131ad4f993d17c192aa873e30592e1030e7794
Caio <whoiscaio@protonmail.com>
2022-02-21 16:11:59 -0300

    feat: register user route added

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,6 +1,57 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user.model');
+
 class usersController {
-  async registerUser(req, res) {
-    res.json({message: 'register new user'});
+  async registerUser(req, res, next) {
+    const { name, email, password } = req.body;
+
+    if(!name) {
+      res.status(400);
+      const error = new Error('Name is a required field');
+      return next(error);
+    }
+
+    if(!email) {
+      res.status(400);
+      const error = new Error('Email is a required field');
+      return next(error);
+    }
+
+    if(!password) {
+      res.status(400);
+      const error = new Error('Password is a required field');
+      return next(error);
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+      res.status(400);
+      const error = new Error('User already exists');
+      return next(error);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const registeredUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    if(!registeredUser) {
+      const error = new Error('Internal error');
+      return next(error);
+    }
+
+    res.status(201).json({
+      _id: registeredUser.id,
+      name: registeredUser.name,
+      email: registeredUser.email,
+    });
   }
 
   async loginUser(req, res) {
