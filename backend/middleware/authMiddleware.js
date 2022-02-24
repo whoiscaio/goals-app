c13fdabf2ce440f56a37af0b62c3debdd656d7fc
Caio <whoiscaio@protonmail.com>
2022-02-24 17:13:38 -0300

    feat: minor adjustment

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,9 +9,9 @@ exports.protect = async (req, res, next) => {
     try {
       token = headerAuth.split(' ')[1];
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select('-password');
+      req.user = await User.findById(id).select('-password');
 
       next();
     } catch (err) {
