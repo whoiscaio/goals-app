f58d91cde645ffec1516401b48ee943c0b5f729b
Caio <whoiscaio@protonmail.com>
2022-02-26 17:07:38 -0300

    feat: useFormError hook

---
+++ frontend/src/hooks/useFormError.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+type ErrorType = {
+  message: string,
+  field: string,
+}
+
+function useFormError() {
+  const [errors, setErrors] = useState<ErrorType[]>([]);
+
+  function setNewError(message: string, field: string) {
+    setErrors((prevState) => [
+      ...prevState,
+      {
+        message: message,
+        field: field,
+      },
+    ])
+  }
+
+  return [errors, setNewError];
+}
+
+export default useFormError;
