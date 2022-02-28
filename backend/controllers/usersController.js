6b1c7722291de6c9ce3660d8f0da68e217e4ec0e
Caio <whoiscaio@protonmail.com>
2022-02-28 14:28:45 -0300

    feat: get user data function updated

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -91,11 +91,7 @@ class usersController {
   }
 
   async getUserData(req, res) {
-    const { _id, name, email } = await User.findById(req.user.id);
-
-    res.status(200).json({
-      id: _id, name, email
-    })
+    res.status(200).json(req.user);
   }
 }
 
