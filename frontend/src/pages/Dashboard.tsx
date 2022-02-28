5a648a60bde6b27460b795790ba3d85f60be912f
Caio <whoiscaio@protonmail.com>
2022-02-28 14:32:36 -0300

    feat: forbidden dashboard for unlogged users

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { RootState } from '../store/store';
+
 function Dashboard() {
+  const { user } = useSelector((state: RootState) => state.auth);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return <div>Dashboard</div>;
 }
 
